bed4200e10b68f4688e6df604c4f360bc44f033c
Nanda <mariafsguedes@fac.pe.senac.br>
2022-06-03 20:43:11 -0300

    Co-authored-by: Luan Vinicius Monteiro Dos Santos Silva <AtlasGold@users.noreply.github.com>

--- wrapper.js
+++ wrapper.js
@@ -1,22 +1,21 @@
 function wrapper(num){
+ 
+        num = process.argv[2]
+        const number1 = require('extenso');
 
-    num = process.argv[2]
-    const number1 = require('extenso');
+        const number2 = require('numero-por-extenso');//não achou essa extensção
 
-    const number2 = require('numero-por-extenso');
+        const number3 = require('number-to-words');
 
-    const number3 = require('number-to-words');
 
-    const number4 = require('por-extenso');
-    
-    
-                 
-    console.log(number1(num))
-    console.log(number2.porExtenso(num))
-    console.log(number3.toWords(num))
-    console.log(number4(num))
+        
+        
+                    
+        console.log(number1(num)) 
+        console.log(number2.porExtenso(num))
+        console.log(number3.toWords(num))
+   
 
 }
-
 
 wrapper()
