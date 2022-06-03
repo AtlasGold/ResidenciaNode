0b5959291cca53ba64e695f47aff75b2d0f4b1ab
Nanda <mariafsguedes@fac.pe.senac.br>
2022-06-03 12:58:49 -0300

    tentativa de fazer a função

--- wrapper.js
+++ wrapper.js
@@ -1,6 +1,16 @@
-const number1 = require('extenso')
 
-const number2 = require('numero-por-extenso');
+//console.log(number1(22))
+//console.log(number2.porExtenso(33))
 
-console.log(number1(22))
-console.log(number2.porExtenso(33))
+
+function wrapper(num){
+
+    const number1 = require('extenso')
+
+    const number2 = require('numero-por-extenso');
+    
+    return number1(num), number2.porExtenso(num)
+}
+
+
+console.log(wrapper())
